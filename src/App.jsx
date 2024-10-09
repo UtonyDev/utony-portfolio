d21f7376ddc6adbc4b0b19fc66e6e7e1cb5dd49f
UtonyDev <udoyeanthony948@gmail.com>
2024-10-09 19:01:02 +0100

    testing troubleshoot for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import UTonyCalc from './Pages/utonycalc/utonycalc';
-import UHomePage from './Pages/uhomepage';
-import Header from './Components/header';
-import Footer from './Components/footer';
-import './App.css';
-
-function App() {
-    
-  return (
-<Router basename="/utony-portfolio">
-      <Header />
-      <Routes>
-        <Route path="*" element={<UHomePage  />} />
-        <Route path="/utonycalc" element={<UTonyCalc />} />
-      </Routes>
-      <Footer />
-    </Router>
-    
-  );
-}
+const App = () => {
+  return <h1>Hello World</h1>;
+};
 
 export default App;
