84926d80cdc36b780b7038e0fbbe9785c4015386
UtonyDev <udoyeanthony948@gmail.com>
2024-12-26 22:12:09 +0100

    Working on prompt UI for Weather app

--- src/Pages/utonyweather/locationForm.jsx
+++ src/Pages/utonyweather/locationForm.jsx
@@ -18,16 +18,19 @@ function LocationForm({ fetchData }) {
     }
 
     return (
-        <div>
-            <h1>Weather App</h1>
-            <form onSubmit={handleSubmit}>
-                <input className='cityInput' type="text" placeholder="City" value={city} 
+        <div className='place-content-center gridborder-2 border-indigo-500 rounded'>
+            <h1><span className="gb hid p-0 py-3">U</span>Tony Weather</h1>
+            <form onSubmit={handleSubmit} className='grid grid-rows-4 gap-2'>
+                Enter Location 
+                <input className='mx-3 p-2' type="text" placeholder="City" value={city} 
                 onChange={(e) => setCity(e.target.value)} />
                 
-                <input country='countryInput' type="text" placeholder="Country" value={country} 
+                <input className='mx-3 p-2' type="text" placeholder="Country" value={country} 
                 onChange={(e) => setCountry(e.target.value)} />
                 
-                <button type="submit">Get Weather</button>
+                <button className='mx-3 my-3 p-2 bg-teal-900' type="submit"> Enter </button>
+                <button className='mx-3 my-3 p-2 bg-teal-900' type="submit">Use Location</button>
+
             </form>
         </div>
     );
