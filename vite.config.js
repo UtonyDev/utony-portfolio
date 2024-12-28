cb5557a4318cab6ee04590ef18dbc5b68d289e33
UtonyDev <udoyeanthony948@gmail.com>
2024-12-28 19:52:50 +0100

    Attempting Versioning 1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    outDir: 'dist', // The directory for build output
+    assetsDir: 'assets', // Where to store hashed assets
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
 });
